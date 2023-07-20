32e7b3a9d2c06488d9cd237a31056ff4ffc66d65
1337-server <sndspamfilter@gmail.com>
2023-07-20 07:27:25 +0100

    Allow update of Modal.vue from homepage buttons

--- src/components/database/Modal.vue
+++ src/components/database/Modal.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Search Database</h5>
+          <h5 class="modal-title" id="exampleModalLongTitle">{{ title }}</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
@@ -28,10 +28,13 @@
     </div>
   </div>
 </template>
-<script>
-export default {
-  name: 'modal'
-}
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: false
+  }
+})
 </script>
 <style>
 .modal {
